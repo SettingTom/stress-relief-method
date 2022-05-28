7228de774d6267a13436a046c3234a8355f6d959
SettingTom <tnbt0277@gmail.com>
2022-05-28 13:50:08 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   get 'methods/index'
   root to: "methods#index"
   resources :methods, only: [:index]
-  get 'gets1', to: 'methods#positive_memories'
-  get 'gets2', to: 'methods#negative_dustbin'
-  get 'gets3', to: 'methods#forehead_tapping'
-  get 'gets4', to: 'methods#mindbus_technique'
-  get 'gets5', to: 'methods#mental_clearbutton'
-  get 'gets6', to: 'methods#fry_on_the_wall'
-  get 'gets7', to: 'methods#mental_subtracting.html'
-  get 'gets8', to: 'methods#fivesense_countdown'
+  get 'gets1st1', to: 'methods#stress_surmometer'
+  get 'gets1',    to: 'methods#positive_memories'
+  get 'gets2',    to: 'methods#negative_dustbin'
+  get 'gets3',    to: 'methods#forehead_tapping'
+  get 'gets4',    to: 'methods#mindbus_technique'
+  get 'gets5',    to: 'methods#mental_clearbutton'
+  get 'gets6',    to: 'methods#fry_on_the_wall'
+  get 'gets7',    to: 'methods#mental_subtracting.html'
+  get 'gets8',    to: 'methods#fivesense_countdown'
 end
