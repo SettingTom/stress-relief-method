b801972dca41130fe6d071529168fc162582ba7f
SettingTom <tnbt0277@gmail.com>
2022-05-29 18:27:11 +0900

    ストップボタン実装

--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -2,10 +2,15 @@ window.onload = function(){
   let time = 14;
   let counter;
   let start = document.getElementById("start");
+  let stop = document.getElementById("stop");
 
   start.onclick = function() {
   toggle();
   counter = setInterval( count, 1000 );
   }
 
+  stop.onclick = function() {
+    toggle();
+    clearInterval( counter );
+    }
 } 
