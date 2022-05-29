315bfb468b0ce8d4ece847102afb10180111213f
SettingTom <tnbt0277@gmail.com>
2022-05-29 18:30:17 +0900

    リセットボタン実装

--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -3,14 +3,23 @@ window.onload = function(){
   let counter;
   let start = document.getElementById("start");
   let stop = document.getElementById("stop");
+  let reset = document.getElementById("reset");
+  let sec = document.getElementById("sec");
+  let min = document.getElementById("min");
 
   start.onclick = function() {
-  toggle();
-  counter = setInterval( count, 1000 );
+    toggle();
+    counter = setInterval( count, 1000 );
   }
 
   stop.onclick = function() {
     toggle();
     clearInterval( counter );
-    }
+  }
+
+  reset.onclick = function() {
+    time = 14;
+    sec.innerHTML = time % 60;
+    min.innerHTML = Math.floor( time / 60 );
+  }
 } 
