593b9b2ffab5a81e8ef8f3d8aea1511985e12f58
SettingTom <tnbt0277@gmail.com>
2022-06-02 23:32:06 +0900

    信号送信アニメーション実装

--- app/javascript/signal.js
+++ app/javascript/signal.js
@@ -2,11 +2,22 @@ window.addEventListener('load', () => {
   const signal_check = document.getElementById("signal_check");
   
   signal_check.addEventListener("click", () => {
-
-    if(signal_anime.classList.contains("signal_anime")){
-      signal_anime.classList.remove("signal_anime");
+    if(signal_anime1.classList.contains("signal_anime1")){
+      signal_anime1.classList.remove("signal_anime1");
     }else{
-      signal_anime.classList.add("signal_anime");
+      signal_anime1.classList.add("signal_anime1");
+    }
+
+    if(signal_anime2.classList.contains("signal_anime2")){
+      signal_anime2.classList.remove("signal_anime2");
+    }else{
+      signal_anime2.classList.add("signal_anime2");
+    }
+
+    if(signal_anime3.classList.contains("signal_anime3")){
+      signal_anime3.classList.remove("signal_anime3");
+    }else{
+      signal_anime3.classList.add("signal_anime3");
     }
   })
 });
