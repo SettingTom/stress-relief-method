8dcf668bc585f8b2db747318069d29ce53c2ed3b
SettingTom <tnbt0277@gmail.com>
2022-06-16 07:51:18 +0900

    文字代入の方法の修正

--- app/javascript/negative_thinking_list.js
+++ app/javascript/negative_thinking_list.js
@@ -2,6 +2,6 @@ window.addEventListener('load', () => {
   const sadInput = document.getElementById("sad");
   const fillLabel = document.getElementById("think");
   sadInput.addEventListener("click", () => {
-    fillLabel.innerHTML = ("悲しみ")
+    fillLabel.innerHTML = (sadInput.value)
   });
 });
