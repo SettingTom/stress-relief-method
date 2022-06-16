fd5998706e1b5b3ba38b8a508d73a1be87f38f51
SettingTom <tnbt0277@gmail.com>
2022-06-16 11:35:49 +0900

    クリックした文字を文章に代入する機能の実装

--- app/javascript/negative_thinking_list.js
+++ app/javascript/negative_thinking_list.js
@@ -1,7 +1,38 @@
 window.addEventListener('load', () => {
   const sadInput = document.getElementById("sad");
+  const shameInput = document.getElementById("shame");
+  const anxietyInput = document.getElementById("anxiety");
+  const disgustInput = document.getElementById("disgust");
+  const angerInput = document.getElementById("anger");
+  const guiltInput = document.getElementById("guilt");
+  const frustrationInput = document.getElementById("frustration");
   const fillLabel = document.getElementById("think");
+
   sadInput.addEventListener("click", () => {
     fillLabel.innerHTML = (sadInput.value)
+  });
+
+  shameInput.addEventListener("click", () => {
+    fillLabel.innerHTML = (shameInput.value)
+  });
+
+  anxietyInput.addEventListener("click", () => {
+    fillLabel.innerHTML = (anxietyInput.value)
+  });
+
+  disgustInput.addEventListener("click", () => {
+    fillLabel.innerHTML = (disgustInput.value)
+  });
+
+  angerInput.addEventListener("click", () => {
+    fillLabel.innerHTML = (angerInput.value)
+  });
+
+  guiltInput.addEventListener("click", () => {
+    fillLabel.innerHTML = (guiltInput.value)
+  });
+
+  frustrationInput.addEventListener("click", () => {
+    fillLabel.innerHTML = (frustrationInput.value)
   });
 });
